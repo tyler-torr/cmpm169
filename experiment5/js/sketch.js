7ca7cf20f41b0b3cf1a8e47380885542eaf3c076
tyler-torr <tylertorrella@gmail.com>
2025-02-10 19:22:00 -0800

    Change resizeCanvas
    Accidentally added an unnecessary parameter to resizeCanvas

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -35,7 +35,7 @@ function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
   console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
   // redrawCanvas(); // Redraw everything based on new size
 }
 
